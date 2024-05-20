5f4d07c3c9fb8eefd51918eae4ba8dbacaa8b0cd
Carlos Vaz <carlos@carjorvaz.com>
2024-05-20 18:31:53 +0100

    re-enable projectile caching

--- config.el
+++ config.el
@@ -71,7 +71,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq projectile-enable-caching nil)
 (setq delete-by-moving-to-trash t)
 
 ;; Make the Sly REPL pop-up on the side instead of on the bottom
