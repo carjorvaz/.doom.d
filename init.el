6121896c58ef552b5ce716a73009de8457f26628
Carlos Vaz <carlos@carjorvaz.com>
2024-09-02 10:28:15 +0100

    change completion flags

--- init.el
+++ init.el
@@ -22,11 +22,11 @@
 
        :completion
        ;;(company +childframe)           ; the ultimate code completion backend
-       corfu             ; complete with cap(f), cape and a flying feather!
+       (corfu +orderless)             ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)           ; the search engine of the future
+       vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
